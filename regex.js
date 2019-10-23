600cc8eda082aad7044e564ed75f56e13e0b317a
Tony Ramirez <tony@Tonys-MacBook-Pro.local>
2019-10-23 00:32:20 -0600

    working

--- regex.js
+++ regex.js
@@ -1,51 +1,73 @@
+	/*
+	 URL RegEx Generator
+	 Created by Tony Ramirez
+	 -----------------------------------------------------------
+	 Paste all your urls seperated by white space and hit submit.
+	 This code will generate RegEx to select all of your urls.
+	 ------------------------------------------------------------
+	 Helpful for Google Analytics, Google Optimize, GTM, and 
+		other conversion trackers
+	 */
+console.log("RegEx Generator 1.0.0");
 
-document.querySelector('#url_submit-btn').click(() => {
-	event.location.preventDefault();
-});
+document.querySelector('.form_submit-btn').addEventListener('click', () => {
+	event.preventDefault();
+	//Get URLS from input
+	var urls = document.querySelector('#url_input-area').value;
+	//Splits the URLS by space(s) newline(s) and adds them to an array
+	var url_split = urls.split(/\s+/);
+	//RegEx will be added to this string
+	var regex_string = '';
+	//Boolean for if there is an empty element in the array
+	var empty_element = false;
 
+	//Begin error checking and generating RegEx
+	try {
+		//Counts Url
+		const url_count = url_split.length - 1;
+		console.log("URL Count:", url_count);
 
-var urls = "https://www.hello.com/my_hello_domain/?red+3=204 HTTP://www.bobsburger.com/thank-you/hello(329)&&& https://www.hello.com/shop/sneakers/blue?9838202&red=20003 http://www.myname.com/index.html http://sub.domain.net/?param1=foo/";
+		url_split.forEach((el, i) => {
+			//Error Testing
+			var url_check =	el.search(/(http|https):\/\/[\w-]+\.[\w-]+\.\w+/i);
+			if (url_check == -1)  { //-1 means there was no match, therefore no complete url.
+				if (!el){
+					empty_element = true; //Checks for empty array (The exception to this url_check)
+				} 
+				else {
+					throw `One or more URLs aren't a complete URL path. -- Check URL number: ${i}`; 
+				}
+			}
 
-//Splits the URLS by space(s) newline(s) and adds them to an array
-var url_split = urls.split(/\s+/);
+			//Cut domain out of url
+			el = el.replace(/(http|https):\/\/[\w-]+\.[\w-]+\.\w+(\/)?/i, '/');	
 
-//Regex Will be added to this string
-var regex_string = '';
+			//If there is only a '/' is the url, that means there's no path. So don't make regex for it.
+			if ( el.length == '1') return; 
+			regex_string = regex_string.concat(el, '|');
+		});
 
-try {
-
-	url_split.forEach(el => {
-		//Error Testing
-		var url_check =	el.search(/(http|https):\/\/[\w-]+\.[\w-]+\.\w+/i);
-		if (url_check == -1) throw "One or more URLS aren't a complete URL path. e.g. https://www.mywebsite.com -- Please include the full URL"; //-1 means there was no match, therefore no complete url
-
-		//Cut domain out of url
-		el = el.replace(/(http|https):\/\/[\w-]+\.[\w-]+\.\w+(\/)?/i, '/');	
-
-		//If there is only a '/' is the url, that means there's no path. So don't make regex for it.
-		if ( el.length == '1') return; 
-		regex_string = regex_string.concat(el, '|');
-
-	});
-
-	//remove extra "|" from end of string
-	regex_string = regex_string.slice(0, -1);
-
-	//escape regex characters e.g. ? . + / ( ) [ ] 
-	var adjustment = 0;
-	[...regex_string].forEach((el, i) => {
-
-		if (el.match(/[\?\.\+\/\(\)\[\]]/)) {
-			regex_string = regex_string.slice(0, i + adjustment) + "\\" + regex_string.slice(i + adjustment);
-			adjustment++;
+		//remove extra "|" from end of string
+		regex_string = regex_string.slice(0, -1);
+		//Delete 2 things off the end because of empty element bug
+		if (empty_element) {
+			regex_string = regex_string.slice(0, -1); 
 		}
 
-	});
+		//escape regex characters e.g. ? . + / ( ) [ ] 
+		var adjustment = 0;
+		[...regex_string].forEach((el, i) => {
 
-	console.log(regex_string);
+			if (el.match(/[\?\.\+\/\(\)\[\]]/)) {
+				regex_string = regex_string.slice(0, i + adjustment) + "\\" + regex_string.slice(i + adjustment);
+				adjustment++;
+			}
 
-} catch(err) {
-	console.log(`Error: ${err}`);
-}
+		});
 
+		document.querySelector('#url_output-area').value = regex_string;
 
+	} catch(err) {
+		document.querySelector('#url_output-area').value = err;
+	}
+});
